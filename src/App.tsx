cb58943851ed27cc920b03785a3372739054f3f7
danielromeo <danielromeo99@gmail.com>
2024-11-29 07:34:03 +0200

    added styles to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, Container, Row, Col } from 'react-bootstrap';
-
+import { Container, Row, Col, Card, Table } from 'react-bootstrap';
+import pricingData from './pricing-data.json';
+import styles from './styles/pricing.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          <Button variant="primary">Bootstrap Button</Button>
+    <Container className={styles.pricingpage}>
+      <h1 className="text-center my-5">Check out our prices</h1>
+
+      <Row className="mb-5">
+        <Col md={4}>
+          <Card className="pricing-card">
+            <Card.Header>Rugs</Card.Header>
+            <Card.Body>
+              <Table striped bordered>
+                <thead>
+                  <tr>
+                    <th>Sizes</th>
+                    <th>Prices</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pricingData.rugs.sizes.map((size, index) => (
+                    <tr key={index}>
+                      <td>{size}</td>
+                      <td>R {pricingData.rugs.prices[index].toFixed(2)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
         </Col>
+
+        {/* Repeat similar structure for other pricing sections */}
+
       </Row>
     </Container>
-    </div>
   );
 }
 
